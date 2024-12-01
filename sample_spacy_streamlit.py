4274358582e028ed6a673145094c9a47fc509abe
osamahabib5 <54497174+osamahabib5@users.noreply.github.com>
2024-12-01 15:54:06 +0500

    Making this commit for git , ignoring the myenv folder

--- sample_spacy_streamlit.py
+++ sample_spacy_streamlit.py
@@ -1,25 +1,5 @@
+import numpy as np
 import spacy
-import spacy_streamlit
-import streamlit as st
 
-# List of SpaCy models to choose from
-models = ["en_core_web_sm", "en_core_web_md", "en_core_web_lg"]
-
-# Streamlit interface for selecting a model
-selected_model = st.selectbox("Select SpaCy Model:", models)
-
-# Load the selected model
-@st.cache_resource
-def load_model(model_name):
-    return spacy.load(model_name)
-
-nlp = load_model(selected_model)
-
-# Streamlit interface for text input
-st.title("Named Entity Recognition with spaCy Streamlit")
-text = st.text_area("Enter text for NER:")
-
-if text:
-    doc = nlp(text)
-    # Visualize NER with spacy-streamlit
-    spacy_streamlit.visualize_ner(doc, labels=nlp.get_pipe("ner").labels)
+print("Numpy version:", np.__version__)
+print("spaCy version:", spacy.__version__)
